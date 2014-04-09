05de2b6210740fc0c12eb87d7a9a2194f0e8c7e1
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-04-09 19:38:37 +0900

    Update the stub file.

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,3 +1,18 @@
-class Stub {
-  // got any helpful boilerplate for your users?
+import java.util.Scanner
+import java.io.PrintWriter
+import scala.annotation._
+import scala.collection.mutable
+import scala.util.Random
+
+class Testcase(sc: Scanner) {
+  // do something
+}
+
+object Stub extends App {
+  val sc = new Scanner(System.in)
+  val out = new PrintWriter(System.out)
+
+  // do something
+
+  out.close
 }
