87b23175d22f3bb613a493c007c909e67df69935
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-04-26 09:50:59 +0900

    Import some extra collection classes.

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,9 +1,13 @@
 import java.util.Scanner
 import java.io.PrintWriter
-import scala.annotation._
+import java.util.Arrays.binarySearch
+import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.PriorityQueue
 import scala.util.Random
-import scala.util.matching.Regex
+import scala.util.control.Breaks._
 
 object Stub extends App {
   val sc = new Scanner(System.in)
