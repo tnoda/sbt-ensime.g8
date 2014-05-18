a32ea580379a9bd1d21d95dafd7617f35bc49959
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-05-18 17:58:24 +0900

    Add the Input object.

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,4 +1,6 @@
 import java.util.Scanner
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.lang.Long.bitCount
 import java.nio.CharBuffer
@@ -14,8 +16,45 @@ import scala.util.Random
 import scala.util.control.Breaks._
 
 object Stub extends App {
-  val sc = new Scanner(System.in)
+  // val sc = new Scanner(System.in)
   val out = new PrintWriter(System.out)
+
+  object Input {
+    import java.lang.Character._
+
+    lazy val in = new BufferedReader(new InputStreamReader(java.lang.System.in))
+    var buf: Array[Char] = Array()
+    var pos: Int = 0
+
+    def next(): String = {
+      while (pos == buf.length) {
+        buf = in.readLine.toArray
+        pos = 0
+      }
+      while (isWhitespace(buf(pos))) {
+        pos += 1
+      }
+      while (pos == buf.length) {
+        buf = in.readLine.toArray
+        pos = 0
+      }
+      val s = pos
+      while (pos < buf.length && !isWhitespace(buf(pos))) {
+        pos += 1
+      }
+      new java.lang.String(buf, s, pos - s)
+    }
+
+    def nextInt(): Int = next.toInt
+
+    def nextLong(): Long = next.toLong
+
+    def nextFloat(): Float = next.toFloat
+
+    def nextDouble(): Double = next.toDouble
+
+    val nextString: () => String = next _
+  }
 
   // do something
 
