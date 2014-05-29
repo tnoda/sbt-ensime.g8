cf1ed2208666bb6b85fbe79dd13ab770475426e3
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-05-29 16:33:51 +0900

    Add modulo operations.

--- src/main/g8/src/main/scala/PrimeNumber.scala
+++ src/main/g8/src/main/scala/PrimeNumber.scala
@@ -34,3 +34,48 @@ object PrimeNumber {
     primes.toArray
   }
 }
+
+class PrimeNumber(val p: Long) {
+
+  def modPow(x: Long, n: Long): Long = {
+
+    @tailrec
+    def loop(acc: Long, x: Long, n: Long): Long = {
+      if (n == 0) acc
+      else if ((n & 1) == 0) loop(acc, x * x % p, n >> 1)
+      else loop(acc * x % p, x * x % p, n >> 1)
+    }
+
+    loop(1, x, n)
+  }
+
+  def modInv(x: Long): Long = modPow(x, p - 2)
+
+  // Partial permutation aPb
+  def modPermu(a: Long, b: Long): Long = {
+    
+    @tailrec
+    def loop(acc: Long, i: Long, j: Long): Long = {
+      if (j == 0) acc
+      else loop(acc * i % p, i - 1, j - 1)
+    }
+    
+    loop(1, a, b)
+  }
+
+  def modFact(x: Long): Long = modPermu(x, x)
+
+  def modInvFact(x: Long): Long = {
+
+    @tailrec
+    def loop(acc: Long, x: Long): Long = {
+      if (x < 2) acc
+      else loop(acc * modInv(x) % p, x - 1)
+    }
+
+    loop(1, x)
+  }
+
+  // Compbination aCb
+  def modCombi(a: Long, b: Long): Long = modPermu(a, b) * modInvFact(b) % p
+}
